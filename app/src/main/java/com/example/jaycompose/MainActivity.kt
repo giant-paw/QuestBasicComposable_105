ab6cb695362e7338044844af5b3ac3456a960eed
giant-paw <giantpawbusiness@gmail.com>
2024-10-16 16:35:45 +0700

    Menambahkan foto di folder 'drawable' dan menampilkannya di fungsi BasicCompound.

--- app/src/main/java/com/example/jaycompose/MainActivity.kt
+++ app/src/main/java/com/example/jaycompose/MainActivity.kt
@@ -4,21 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
 import com.example.jaycompose.ui.theme.JayComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -51,6 +56,11 @@ fun BasicCompound(modifier: Modifier = Modifier) {
             fontStyle = FontStyle.Italic,
             modifier = Modifier.padding(top = 30.dp)
         )
+
+        Image(painter = painterResource(id = R.drawable.logo),
+            contentDescription = "",
+        )
+
     }
 }
 
