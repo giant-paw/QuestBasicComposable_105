2d749743c84dbacbbdfd704bb876439609333d88
giant-paw <giantpawbusiness@gmail.com>
2024-10-16 16:11:21 +0700

    Mengatur teks dan element agar di tengah (center) dan fillMaxSize.

--- app/src/main/java/com/example/jaycompose/MainActivity.kt
+++ app/src/main/java/com/example/jaycompose/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jaycompose.ui.theme.JayComposeTheme
 
@@ -32,9 +35,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicCompound(modifier: Modifier = Modifier) {
-    Column {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier.fillMaxSize()
+    ) {
         Text(
-            text = "LOGIN"
+            text = "LOGIN",
         )
     }
 }
